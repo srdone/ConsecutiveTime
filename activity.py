4b623f08d68f0f2431f53ed2daf5bddb184a0d42
Stephen Done <srdone@gmail.com>
2014-05-31 09:09:15 -0600

    Added mutliple tests to person.py. Modified activity to accept dates and strings for the date object. Sorting is broken for courses with same start dates but different end dates.

--- activity.py
+++ activity.py
@@ -12,8 +12,16 @@ class Activity(object):
     def __init__(self, location, activity_name, activity_id, start_date, end_date, pattern='%m/%d/%Y'):
         self.activity_name = activity_name
         self.activity_id = activity_id
-        self.start_date = datetime.strptime(start_date, pattern).date()
-        self.end_date = datetime.strptime(end_date, pattern).date()
+        if type(start_date) == str:
+        	self.start_date = datetime.strptime(start_date, pattern).date()
+        else:
+        	#assumes start date is a date. need error handling
+        	self.start_date = start_date
+        if type(end_date) == str:
+        	self.end_date = datetime.strptime(end_date, pattern).date()
+        else:
+        	#assumes end date is a date. need error handling
+        	self.end_date = end_date
         self.location = location
 	
 class TestActivity(unittest.TestCase):
