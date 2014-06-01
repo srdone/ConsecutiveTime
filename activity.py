398307278e8e46bc61da162356f100a7a1e353b0
Stephen Done <srdone@gmail.com>
2014-05-31 22:20:04 -0600

    Added properties to activity.py for start and end dates.

--- activity.py
+++ activity.py
@@ -8,21 +8,42 @@ import unittest
 from datetime import datetime, timedelta
 
 class Activity(object):
-    'Holds detail information on a activity'
-    def __init__(self, location, activity_name, activity_id, start_date, end_date, pattern='%m/%d/%Y'):
-        self.activity_name = activity_name
-        self.activity_id = activity_id
-        if type(start_date) == str:
-        	self.start_date = datetime.strptime(start_date, pattern).date()
-        else:
-        	#assumes start date is a date. need error handling
-        	self.start_date = start_date
-        if type(end_date) == str:
-        	self.end_date = datetime.strptime(end_date, pattern).date()
-        else:
-        	#assumes end date is a date. need error handling
-        	self.end_date = end_date
-        self.location = location
+	'Holds detail information on a activity'
+	
+	def __init__(self, location, activity_name, activity_id, start_date, end_date, pattern='%m/%d/%Y'):
+		self._start_date = None
+		self._end_date = None
+		
+		self.activity_name = activity_name
+		self.activity_id = activity_id
+		self.pattern = pattern
+		self.start_date = start_date
+		self.end_date = end_date
+		self.location = location
+		
+	@property
+	def start_date(self):
+		return self._start_date
+	
+	@start_date.setter
+	def start_date(self, start_date):
+		if type(start_date) == str:
+			self._start_date = datetime.strptime(start_date, self.pattern).date()
+		else:
+			#assumes start date is a date. need error handling
+			self._start_date = start_date
+	
+	@property
+	def end_date(self):
+		return self._end_date
+	
+	@end_date.setter
+	def end_date(self, end_date):
+		if type(end_date) == str:
+			self._end_date = datetime.strptime(end_date, self.pattern).date()
+		else:
+			#assumes end date is a date. need error handling
+			self._end_date = end_date
 	
 class TestActivity(unittest.TestCase):
 	
